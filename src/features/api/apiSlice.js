c8abead2fb1e05c0eec785cc57ba1d6694c8c766
Cyber9ja <ayodejioladapo96@gmail.com>
2023-08-21 18:34:26 +0100

    minor update

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,10 +1,9 @@
+/* eslint-disable no-unused-vars */
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https:localhost:3500'}),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:3500'}),
   tagTypes: ["Post"],
-  endpoints: ( builder ) => ({
-    
-  })
+  endpoints: builder => ({})
 })
--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -2,3 +2,31 @@
 import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
 import { sub } from 'date-fns';
 import { apiSlice } from "../api/apiSlice";
+
+
+// A function that generates a set of prebuilt reducers and selectors for performing CRUD operations 
+// on a normalized state structure containing instances of a particular type of data object
+// createEntityAdapter accepts a single options object parameter, with two optional fields inside.
+
+const postsAdapter = createEntityAdapter({
+
+  // Sort all posts by date from the newest to the oldest
+  sortComparer: (a, b) => b.date.localCompare(a.date), // Check documentation
+});
+
+const initialState = postsAdapter.getInitialState();
+
+// InjectedEndpoint enables code splitting by injecting the endpoint into the original API
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  endpoints: (build) => ({
+    getPosts: build.query({
+      query: () => '/posts',
+      transformResponse: responseData => {   // This works exactly like interceptors in axios
+        let min = 1;
+        const loadedPosts = responseData.map(post => {
+          if(!post?.date) { post.date = sub(new Date(), { minutes: min })}
+        })
+      }
+    })
+  })
+})
